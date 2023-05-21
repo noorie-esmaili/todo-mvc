90352fc426ea2c44cb8d44326e4bd8c7560a07b9
Noorie Esmaili <noorie1380esmaili@gmail.com>
2023-05-21 22:56:36 +0200

    make todo item a checkbox

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,21 +1,28 @@
-
-// TodoInput.tsx
-import React from "react";
+import React, { useState } from "react";
 
 interface TodoItemProps {
-    title: string
+    title: string;
 }
 
 const TodoItem: React.FC<TodoItemProps> = ({ title }) => {
+    const [checked, setChecked] = useState(false);
+
+    const handleCheckboxChange = () => {
+        setChecked(!checked);
+    };
+
     return (
-        <h5>{title}</h5>
+        <div>
+            <input
+                type="checkbox"
+                checked={checked}
+                onChange={handleCheckboxChange}
+            />
+            <label style={{ textDecoration: checked ? "line-through" : "none" }}>
+                {title}
+            </label>
+        </div>
     );
 };
 
 export default TodoItem;
-
-
-
-
-
-
