d050552d02ff61b5cace5083c9925af750a0d85f
Noorie Esmaili <noorie1380esmaili@gmail.com>
2023-05-21 21:17:02 +0200

    Render Home page

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Home } from './pages/home.tsx'
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" Component={Home} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/pages/home.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Home: React.FC = () => {
+    return (
+        <div>
+            <h1>Todo App</h1>
+            {/* Add your todo app components and functionality here */}
+        </div>
+    );
+};
